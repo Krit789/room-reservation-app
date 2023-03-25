6668872ad3da711a131e53cdd86821c6d69c3af5
EntityKunG <sharpkung4826@gmail.com>
2023-03-25 22:57:11 +0700

    Test FewConfig complete naja

--- src/net/itkmitl/room/ApplicationMain.java
+++ src/net/itkmitl/room/ApplicationMain.java
@@ -3,11 +3,17 @@ package net.itkmitl.room;
 import net.itkmitl.room.libs.peeranat.config.*;
 
 import java.io.*;
+import java.util.*;
 
 public class ApplicationMain {
 
     public static void main(String[] args) {
         FewConfig config = new FewConfig(new File("test.yml"));
-        config.dumpInfo();
+        System.out.println(config.asString("name"));
+        System.out.println(config.asInt("phone"));
+        System.out.println(config.asDouble("point"));
+        System.out.println(config.asDouble("kuy", 0.0));
+        List<String> list = config.asList("x", String.class);
+        list.stream().forEach(System.out::println);
     }
 }
