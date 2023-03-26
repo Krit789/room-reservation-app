2a4c5c24e35c634bc9427e8d28c0e823ae9daef6
TaeTanakrit0089 <tanakritsupprasit@gmail.com>
2023-03-26 17:09:55 +0700

    I can solve this shit!!!

--- src/main/resources/Login.java
+++ src/main/resources/Login.java
@@ -1,17 +1,21 @@
-package main.java.gui.login;
+package main.resources;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Login extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("file:src/main/resources/gui/login/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        Parent root = FXMLLoader.load(getClass().getResource("/hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
