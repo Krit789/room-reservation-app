7f0a37991307b3dd7f53aa6db6d8a277436ca5c5
Jarukrit <jarukritsripaploen@protonmail.com>
2023-03-26 21:33:46 +0700

    Caught up

--- src/net/itkmitl/room/GUIStarter.java
+++ src/net/itkmitl/room/GUIStarter.java
@@ -1,9 +1,12 @@
 package net.itkmitl.room;
 
+import main.resources.account.Login;
+
 public class GUIStarter {
 
     public static void main(final String[] args) {
-        SwingNodeExample.main(args);
+//        SwingNodeExample.main(args);
+        Login.main(args);
     }
 
 }
