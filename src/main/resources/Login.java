28e29ac85d0aaf987d7b0d2c31633f4bbddea697
EntityKunG <sharpkung4826@gmail.com>
2023-03-26 17:34:18 +0700

    Use the dependency JavaFX on version 17.0x

--- src/main/resources/Login.java
+++ src/main/resources/Login.java
@@ -1,19 +1,16 @@
 package main.resources;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.application.*;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.stage.*;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.*;
 
 public class Login extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-//        Parent root = FXMLLoader.load(getClass().getResource("/hello-view.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
