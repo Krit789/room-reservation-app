06e3bfdfb1210ffdde550ade1ffcdc0a719ca256
TaeTanakrit0089 <tanakritsupprasit@gmail.com>
2023-03-26 17:28:12 +0700

    ådded bew account package

--- src/main/resources/account/Login.java
+++ src/main/resources/account/Login.java
@@ -1,9 +1,10 @@
-package main.resources;
+package main.resources.account;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,9 +15,15 @@ public class Login extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 //        Parent root = FXMLLoader.load(getClass().getResource("/hello-view.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("account/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        Image icon = new Image("icon.png");
+        stage.getIcons().add(icon);
+
+        stage.setWidth(1280);
+        stage.setHeight(720);
+        stage.setResizable(false);
+        stage.setTitle("Laew Tae Hong");
         stage.setScene(scene);
         stage.show();
     }
