1e3566b4672c79bfb525c9aee9e89c7326bc805b
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2023-03-27 22:14:25 +0700

    Feat: Feedback Entity and Feedback Repository

---
+++ src/net/itkmitl/room/libs/phatsanphon/repository/FeedbackRepository.java
@@ -0,0 +1,106 @@
+package net.itkmitl.room.libs.phatsanphon.repository;
+
+import net.itkmitl.room.libs.peeranat.query.*;
+import net.itkmitl.room.libs.phatsanphon.entity.Feedback;
+
+import java.util.ArrayList;
+
+public class FeedbackRepository extends Repository<Feedback> {
+    private final FewQuery query;
+
+    public FeedbackRepository(FewQuery query) {
+        super(Feedback.class);
+        this.query = query;
+    }
+
+    public ArrayList<Feedback> getFeedbacks() {
+        FewSelectMySQL select = new FewSelectMySQL();
+
+        select.select("*");
+        select.table("feedback");
+
+        return this.maps(query.query(select));
+    }
+
+    public ArrayList<Feedback> getFeedbacks(int limit) {
+        FewSelectMySQL select = new FewSelectMySQL();
+
+        select.select("*");
+        select.limit(limit);
+        select.table("feedback");
+
+        return this.maps(query.query(select));
+    }
+
+    public Feedback getFeedbackById(int id) {
+        FewSelectMySQL select = new FewSelectMySQL();
+
+        select.where("id", id);
+        select.select("*");
+        select.table("feedback");
+
+        return this.map(query.query(select));
+    }
+
+    public ArrayList<Feedback> getFeedbackByUserId(int userId) {
+        FewSelectMySQL select = new FewSelectMySQL();
+
+        select.where("user_id", userId);
+        select.select("*");
+        select.table("feedback");
+
+        return this.maps(query.query(select));
+    }
+
+    public ArrayList<Feedback> getFeedbackByRoomId(int roomId) {
+        FewSelectMySQL select = new FewSelectMySQL();
+
+        select.where("user_id", roomId);
+        select.select("*");
+        select.table("feedback");
+
+        return this.maps(query.query(select));
+    }
+
+    public void createFeedback(Feedback feedback) {
+        FewInsertMySQL insert = new FewInsertMySQL();
+
+        insert.insert("user_id", feedback.getUser().getId());
+        insert.insert("room_id", feedback.getRoom().getId());
+        insert.insert("comment", feedback.getComment());
+        insert.insert("rating", feedback.getRating());
+        insert.table("feedback");
+
+        query.query(insert);
+    }
+
+    public void updateFeedback(Feedback feedback) {
+        FewUpdateMySQL update = new FewUpdateMySQL();
+
+        if (feedback.getId() == 0) {
+            try {
+                throw new Exception("Please provided an id to update user");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        update.where("id", feedback.getId());
+        update.set("user_id", feedback.getUser().getId());
+        update.set("room_id", feedback.getRoom().getId());
+        update.set("comment", feedback.getComment());
+        update.set("rating", feedback.getRating());
+        update.table("feedback");
+
+        query.query(update);
+    }
+
+    public void deleteFeedbackById(int id) {
+        FewDeleteMySQL delete = new FewDeleteMySQL();
+
+        delete.table("feedback");
+        delete.where("id", id);
+
+        query.query(delete);
+    }
+}
