b446e4e2ad6811a82336b9254b83c9acdf24fcef
TaeTanakrit0089 <tanakritsupprasit@gmail.com>
2023-03-27 22:18:21 +0700

    Improved new Stylesheet and Specific file directory

--- src/net/itkmitl/room/libs/front/user/portal/ControllerPortal.java
+++ src/net/itkmitl/room/libs/front/user/portal/ControllerPortal.java
@@ -1,4 +1,20 @@
 package net.itkmitl.room.libs.front.user.portal;
 
-public class ControllerPortal {
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerPortal implements Initializable {
+    private Pane root;
+
+    private Scene scene;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        root.prefHeightProperty().bind(scene.heightProperty());
+
+    }
 }
