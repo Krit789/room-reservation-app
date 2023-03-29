ebdda7a6e5a4ea3e3f1a745bf90cc68ba33f7f26
TaeTanakrit0089 <tanakritsupprasit@gmail.com>
2023-03-29 11:40:55 +0700

    Uncomplete inheritance class

--- src/net/itkmitl/room/portal/Login.java
+++ src/net/itkmitl/room/portal/Login.java
@@ -1,16 +1,54 @@
 package net.itkmitl.room.portal;
 
-import javafx.scene.control.Button;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import net.itkmitl.room.libs.peeranat.util.*;
+
 
 import java.io.IOException;
 
-public class Login extends Portal{
+public class Login extends Application {
+    protected FXMLLoader fxmlLoader;
+    protected Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        super.start(stage);
-        Button button = new Button("Click me!");
+//        Parent root = FXMLLoader.load(getClass().getResource("/hello-view.fxml"));
+        fxmlLoader = new FXMLLoader(FewFile.getResource("account/Login.fxml"));
+        scene = new Scene(fxmlLoader.load());
+
+//        String css = getClass().getResource("styles/Login.css").toExternalForm();
+//        scene.getStylesheets().add(css);
+
+        Image icon = new Image("icon.png");
+        stage.getIcons().add(icon);
+        stage.setWidth(1280);
+        stage.setHeight(720);
+        stage.setResizable(true);
+        stage.setTitle("Laew Tae Hong");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    protected void runFX(Stage stage) throws IOException {
+//        Parent root = FXMLLoader.load(getClass().getResource("/hello-view.fxml"));
+        fxmlLoader = new FXMLLoader(FewFile.getResource("account/Login.fxml"));
+        scene = new Scene(fxmlLoader.load());
+
+//        String css = getClass().getResource("styles/Login.css").toExternalForm();
+//        scene.getStylesheets().add(css);
+
+        Image icon = new Image("icon.png");
+        stage.getIcons().add(icon);
+        stage.setWidth(1280);
+        stage.setHeight(720);
+        stage.setResizable(true);
+        stage.setTitle("Laew Tae Hong");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
