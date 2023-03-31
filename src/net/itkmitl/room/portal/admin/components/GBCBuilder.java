5c010cf47673841b9ff98b9fcf62848794903108
Jarukrit <jarukritsripaploen@protonmail.com>
2023-03-31 12:55:13 +0700

    Added Status bar and Merged

---
+++ src/net/itkmitl/room/portal/admin/components/GBCBuilder.java
@@ -0,0 +1,11 @@
+package net.itkmitl.room.portal.admin.components;
+
+import java.awt.*;
+
+public class GBCBuilder{
+    private GridBagConstraints gbc;
+    public GBCBuilder(){
+        gbc = new GridBagConstraints();
+    }
+
+}
