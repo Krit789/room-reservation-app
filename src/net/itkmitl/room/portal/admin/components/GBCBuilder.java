9d2714d1944daecea7e7586844c4f15f8e46b4bc
Jarukrit <jarukritsripaploen@protonmail.com>
2023-04-01 02:55:50 +0700

    Feat: GBCBuilder to make GridBagLayout easier to implement

--- src/net/itkmitl/room/portal/admin/components/GBCBuilder.java
+++ src/net/itkmitl/room/portal/admin/components/GBCBuilder.java
@@ -3,9 +3,44 @@ package net.itkmitl.room.portal.admin.components;
 import java.awt.*;
 
 public class GBCBuilder{
-    private GridBagConstraints gbc;
+    private final GridBagConstraints gbc;
+    public GBCBuilder(int fill, double weightx, int gridx, int gridy){
+        gbc = new GridBagConstraints();
+        gbc.fill = fill;
+        gbc.weightx = weightx;
+        gbc.gridx = gridx;
+        gbc.gridy = gridy;
+    }
+    public GBCBuilder(int fill, double weightx, int gridx, int gridy, Insets insets){
+        gbc = new GridBagConstraints();
+        gbc.fill = fill;
+        gbc.weightx = weightx;
+        gbc.gridx = gridx;
+        gbc.gridy = gridy;
+        gbc.insets = insets;
+    }
     public GBCBuilder(){
         gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+    }
+    public GridBagConstraints setInset(Insets insets){
+        gbc.insets = insets;
+        return gbc;
+    }
+
+    public GridBagConstraints getGBC(){
+        return gbc;
+    }
+
+    public GridBagConstraints setPadding(int ipady, int ipadx){
+        gbc.ipady = ipady;
+        gbc.ipadx = ipadx;
+        return gbc;
+    }
+
+    public GridBagConstraints setAnchor(int anchor){
+        gbc.anchor = anchor;
+        return gbc;
     }
 
 }
