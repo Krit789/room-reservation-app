fda6f6398071780fcf79545be622d5602cae4f9c
Jarukrit <jarukritsripaploen@protonmail.com>
2023-04-20 04:35:15 +0700

    Removed unused JavaFX assets

--- src/net/itkmitl/room/portal/admin/components/InternalFrame.java
+++
@@ -1,8 +0,0 @@
-package net.itkmitl.room.portal.admin.components;
-
-import javax.swing.*;
-
-public abstract class InternalFrame {
-    private JInternalFrame frame;
-    public abstract JInternalFrame getFrame();
-}
