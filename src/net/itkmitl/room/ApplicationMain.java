22ed47732694daf712de0344ede63b2fa63eae07
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2023-04-25 11:43:11 +0700

    Fix: DateTime problems

--- src/net/itkmitl/room/ApplicationMain.java
+++ src/net/itkmitl/room/ApplicationMain.java
@@ -2,19 +2,34 @@ package net.itkmitl.room;
 
 import net.itkmitl.room.db.RVDB;
 import net.itkmitl.room.libs.peeranat.query.FewQuery;
+import net.itkmitl.room.libs.phatsanphon.date.DateTime;
 import net.itkmitl.room.libs.phatsanphon.entity.*;
 import net.itkmitl.room.libs.phatsanphon.repository.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ApplicationMain {
     public static void main(String[] args) {
         FewQuery db = RVDB.getDB();
-        RoomRepository roomRepository = new RoomRepository(db);
-        ArrayList<Room> roomList = roomRepository.getRooms();
 
-        for (Room room : roomList) {
-            System.out.println(room.getName());
-        }
+        RoomRepository roomRepository = new RoomRepository(db);
+        FeedbackRepository feedbackRepository = new FeedbackRepository(db);
+
+        Room pjBased = new Room();
+        pjBased.setName("Project Based 1");
+        pjBased.setBuilding("School of IT");
+        pjBased.setFloor("2");
+        pjBased.setOpenTime(new DateTime(new Date(2023 - 1900, 3, 25, 1, 0, 0)));
+        pjBased.setCloseTime(new DateTime(new Date(2023 - 1900, 3, 25, 11, 0, 0)));
+
+        Feedback feedback = new Feedback();
+        feedback.setRoom(7);
+        feedback.setUser(6);
+        feedback.setComment("Good Job!");
+        feedback.setRating(4.5);
+
+//        roomRepository.createRoom(pjBased);
+        feedbackRepository.createFeedback(feedback);
     }
 }
