f1b78c725fd1dce1ece712c794db91a08df4ada5
Jarukrit <jarukritsripaploen@protonmail.com>
2023-05-01 00:42:41 +0700

    Feat: Login Dialog for Admin (User with EnumRole >= 10)

---
+++ src/net/itkmitl/room/portal/admin/controllers/AuthController.java
@@ -0,0 +1,91 @@
+package net.itkmitl.room.portal.admin.controllers;
+
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import net.itkmitl.room.db.RVDB;
+import net.itkmitl.room.libs.peeranat.query.FewQuery;
+import net.itkmitl.room.libs.peeranat.util.FewPassword;
+import net.itkmitl.room.libs.phatsanphon.entity.User;
+import net.itkmitl.room.libs.phatsanphon.repository.UserRepository;
+import net.itkmitl.room.portal.admin.BaseWindow;
+import net.itkmitl.room.portal.admin.views.AuthView;
+import net.itkmitl.room.portal.components.LoadingDialog;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AuthController implements ActionListener {
+    private AuthView view;
+    public static boolean authenticated = false;
+    private Object[] data;
+
+    public AuthController() {
+        view = new AuthView();
+        view.loginButton.addActionListener(this);
+        view.cancelButton.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(view.loginButton)) {
+            view.alertLabel.setText("Processing...");
+            authenticateUser(view.loginField.getText(), String.valueOf(view.passwordField.getPassword()));
+        } else if (e.getSource().equals(view.cancelButton)) {
+            view.getFrame().dispose();
+        }
+    }
+
+    private void authenticateUser(String email, String password){
+        SwingWorker worker = new SwingWorker() {
+            String errorMessage;
+            @Override
+            protected String doInBackground() {
+                try {
+                    FewQuery db = RVDB.getDB();
+                    User myUser = new UserRepository(db).getUserByEmail(email);
+                    if (myUser != null){
+                        if (FewPassword.checkPassword(password, myUser.getPasswordHash()) && myUser.getRole().getLevel() >= 10) {
+                            data = new Object[]{true, myUser};
+                        } else {
+                            view.alertLabel.setText("Invalid Password or Insufficient Permission!");
+                            data = new Object[]{false, "Invalid Password or Insufficient Permission!"};
+                        }
+                    } else {
+                        data = new Object[]{false, "User not found!"};
+                        view.alertLabel.setText("User not found!");
+
+                    }
+                } catch (Exception ex) {
+                    errorMessage = ex.getMessage();
+                    data = new Object[]{false, errorMessage};
+                }
+                return "";
+            }
+            @Override
+            protected void done() {
+                if ((boolean) data[0]){
+                    BaseWindow.main(new String[]{""});
+                    view.getFrame().dispose();
+                } else {
+
+                }
+            }
+        };
+        worker.execute();
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+            System.setProperty("apple.awt.application.name", "Laew Tae Hong");
+            System.setProperty("apple.awt.application.appearance", "system");
+            try {
+                UIManager.setLookAndFeel(new FlatIntelliJLaf());
+            } catch (Exception ignored) {
+                UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+            }
+        } catch (Exception ignored) {
+        }
+        new AuthController();
+    }
+}
---
+++ src/net/itkmitl/room/portal/admin/views/AuthView.java
@@ -0,0 +1,70 @@
+package net.itkmitl.room.portal.admin.views;
+
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import net.itkmitl.room.portal.components.GBCBuilder;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class AuthView {
+    private JFrame frame;
+    private JPanel loginPanel, buttonPanel;
+    public JButton loginButton, cancelButton;
+    private JLabel userLabel, passwordLabel;
+    public JLabel alertLabel;
+    public JTextField loginField;
+    public JPasswordField passwordField;
+
+    public AuthView(){
+        frame = new JFrame("Laew Tae Hong Authentication");
+        ArrayList<Image> multiIcon = new ArrayList<>();
+        multiIcon.add(new ImageIcon("resource/icons/icon-208px.png").getImage());
+        multiIcon.add(new ImageIcon("resource/icons/icon-128px.png").getImage());
+        multiIcon.add(new ImageIcon("resource/icons/icon-64px.png").getImage());
+        multiIcon.add(new ImageIcon("resource/icons/icon-56px.png").getImage());
+        multiIcon.add(new ImageIcon("resource/icons/icon-48px.png").getImage());
+        multiIcon.add(new ImageIcon("resource/icons/icon-40px.png").getImage());
+        multiIcon.add(new ImageIcon("resource/icons/icon-20px.png").getImage());
+        multiIcon.add(new ImageIcon("resource/icons/icon-16px.png").getImage());
+        frame.setLayout(new BorderLayout());
+        frame.add(new JLabel(new ImageIcon("resource/account/banner/banner1-50.png")), BorderLayout.NORTH);
+        frame.setIconImages(multiIcon);
+        frame.setResizable(false);
+        frame.setSize(411, 280);
+
+        loginPanel = new JPanel();
+        loginPanel.setLayout(new GridBagLayout());
+        userLabel = new JLabel("E-Mail");
+        passwordLabel = new JLabel("Password");
+        alertLabel = new JLabel(" ");
+        loginField = new JTextField();
+        passwordField = new JPasswordField();
+        loginPanel.add(userLabel, new GBCBuilder(GridBagConstraints.HORIZONTAL, 0.1, 0, 0, new Insets(10, 10, 5, 5)).getGBC());
+        loginPanel.add(loginField, new GBCBuilder(GridBagConstraints.HORIZONTAL, 0.9, 1, 0, new Insets(10, 0, 5, 10)).getGBC());
+
+        loginPanel.add(passwordLabel, new GBCBuilder(GridBagConstraints.HORIZONTAL, 0.1, 0, 1, new Insets(10, 10, 5, 5)).getGBC());
+        loginPanel.add(passwordField, new GBCBuilder(GridBagConstraints.HORIZONTAL, 0.9, 1, 1, new Insets(10, 0, 5, 10)).getGBC());
+
+        loginPanel.add(alertLabel, new GBCBuilder(GridBagConstraints.HORIZONTAL, 0.1, 1, 2, new Insets(0, 5, 10, 10)).getGBC());
+
+        buttonPanel = new JPanel();
+        buttonPanel.setBorder(new EmptyBorder(0, 0 ,5 ,5));
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        loginButton = new JButton("Login");
+        cancelButton = new JButton("Cancel");
+        buttonPanel.add(loginButton);
+        buttonPanel.add(cancelButton);
+
+        frame.add(loginPanel, BorderLayout.CENTER);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public JFrame getFrame(){
+        return frame;
+    }
+}
