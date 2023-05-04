d363b68b81045498b046992a02a58483c92d63b2
japan0101 <japan.pon11@gmail.com>
2023-05-04 17:27:02 +0700

    add gradient constructor to rounded Panel

--- src/net/itkmitl/room/portal/components/MainSelectionBox.java
+++ src/net/itkmitl/room/portal/components/MainSelectionBox.java
@@ -1,7 +1,16 @@
 package net.itkmitl.room.portal.components;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
-public class MainSelectionBox{
+public class MainSelectionBox extends RoundedPanel{
+    private JLabel timeLabel, nameLabel;
+    //key is floor, Value is arraylist of room in floor
+    private J
+
+    public MainSelectionBox(){
+        super(30, 40, new Color(16, 52, 105), new Color(120, 164, 205));
+
+    }
 }
