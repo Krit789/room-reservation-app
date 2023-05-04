ac30c39fc62ea9b44aaf6fc6055bbf0227aae8d1
TaeTanakrit0089 <tanakritsupprasit@gmail.com>
2023-05-04 15:37:28 +0700

    Added new features and improved design of LoginPanel.java

--- src/net/itkmitl/room/portal/account/components/LoginPanel.java
+++ src/net/itkmitl/room/portal/account/components/LoginPanel.java
@@ -6,9 +6,13 @@ import net.itkmitl.room.portal.components.RoundedPanel;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginPanel extends RoundedPanel {
+public class LoginPanel extends RoundedPanel implements ActionListener, DocumentListener {
     protected JTextField emailField;
     protected JLabel emailText, passwordText, loginHeader, registerLabel1;
     public JLabel warningLabel, registerLabel2;
@@ -18,8 +22,7 @@ public class LoginPanel extends RoundedPanel {
     public ImagePanel imagePanel;
 
 
-
-    public LoginPanel(){
+    public LoginPanel() {
         super(30, 30, Color.WHITE);
         this.setLayout(new GridBagLayout());
         loginButton = new ButtonGradient();
@@ -28,13 +31,21 @@ public class LoginPanel extends RoundedPanel {
         loginButton.setColor1(new Color(44, 102, 188));
         loginButton.setColor2(new Color(94, 135, 197));
 //        registerButton = new JButton("Register");
-        emailField = new JTextField();
-        passwordField = new JPasswordField();
-        emailText = new JLabel("Email", SwingConstants.RIGHT);
-        emailText.setFont(new Font("SansSerif", Font.PLAIN, 16));
 
+        // Email and Password Configuration
+        int textSize = 16, fieldSize = 18;
+        emailText = new JLabel("Email", SwingConstants.RIGHT);
+        emailText.setFont(new Font("SansSerif", Font.PLAIN, textSize));
+        emailField = new JTextField();
+        emailField.setFont(new Font("SansSerif", Font.PLAIN, fieldSize));
         passwordText = new JLabel("Password", SwingConstants.RIGHT);
-        passwordText.setFont(new Font("SansSerif", Font.PLAIN, 16));
+        passwordText.setFont(new Font("SansSerif", Font.PLAIN, textSize));
+        passwordField = new JPasswordField();
+        passwordField.setFont(new Font("SansSerif", Font.PLAIN, fieldSize));
+        emailField.addActionListener(this);
+        emailField.getDocument().addDocumentListener(this);
+        passwordField.addActionListener(this);
+        passwordField.getDocument().addDocumentListener(this);
 
         loginHeader = new JLabel("Login", SwingConstants.CENTER);
         loginHeader.setFont(new Font("SansSerif", Font.BOLD, 48));
@@ -50,7 +61,7 @@ public class LoginPanel extends RoundedPanel {
         loginPanel = new JPanel();
         loginPanel.setBackground(Color.white);
         loginPanel.setLayout(new GridBagLayout());
-        loginPanel.add(loginHeader, new GBCBuilder(GridBagConstraints.NONE, 1, 0, 0, new Insets(5, 0, 5, 45)).setAnchor(GridBagConstraints.WEST));
+        loginPanel.add(loginHeader, new GBCBuilder(GridBagConstraints.NONE, 1, 0, 0, new Insets(15, 0, 40, 45)).setAnchor(GridBagConstraints.WEST));
         loginPanel.add(emailText, new GBCBuilder(GridBagConstraints.NONE, 1, 0, 1, new Insets(5, 0, 5, 45)).setAnchor(GridBagConstraints.WEST));
         loginPanel.add(emailField, new GBCBuilder(GridBagConstraints.HORIZONTAL, 1, 0, 2, new Insets(0, 0, 5, 45)).getGBC());
         loginPanel.add(passwordText, new GBCBuilder(GridBagConstraints.NONE, 1, 0, 3, new Insets(5, 0, 5, 45)).setAnchor(GridBagConstraints.WEST));
@@ -69,16 +80,39 @@ public class LoginPanel extends RoundedPanel {
         operationPanel.add(Box.createRigidArea(new Dimension(35, 0)));
         operationPanel.add(loginButton);
         imagePanel = new ImagePanel(new ImageIcon("resource/account/banner/banner3-entry-25.png"));
-        loginPanel.add(operationPanel, new GBCBuilder(GridBagConstraints.NONE, 1, 0, 7, new Insets(5, 0, 5, 45)).setAnchor(GridBagConstraints.EAST));
+        loginPanel.add(operationPanel, new GBCBuilder(GridBagConstraints.NONE, 1, 0, 7, new Insets(35, 0, 5, 45)).setAnchor(GridBagConstraints.SOUTHEAST));
         this.add(imagePanel, new GBCBuilder(GridBagConstraints.BOTH, 0.35, 1, 0, 0, new Insets(0, 0, 0, 0)).getGBC()); //Image wont go in
-        this.add(loginPanel, new GBCBuilder(GridBagConstraints.HORIZONTAL, 0.65, 1,1, 0, new Insets(0, 25, 0, 60)).setAnchor(GridBagConstraints.NORTHWEST));
-        this.setBorder(new EmptyBorder(25, 25, 25 ,25));
+        this.add(loginPanel, new GBCBuilder(GridBagConstraints.HORIZONTAL, 0.65, 1, 1, 0, new Insets(0, 25, 0, 60)).setAnchor(GridBagConstraints.NORTHWEST));
+        this.setBorder(new EmptyBorder(25, 25, 25, 25));
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return emailField.getText();
     }
-    public String getPassword(){
+
+    public String getPassword() {
         return String.valueOf(passwordField.getPassword());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JTextField perfoemedField = (JTextField) e.getSource();
+        if (perfoemedField.equals(emailField) || perfoemedField.equals(passwordField)) {
+            loginButton.doClick();
+        }
+    }
+
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+        warningLabel.setText("");
+    }
+
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+        warningLabel.setText("");
+    }
+
+    @Override
+    public void changedUpdate(DocumentEvent e) {
     }
 }
