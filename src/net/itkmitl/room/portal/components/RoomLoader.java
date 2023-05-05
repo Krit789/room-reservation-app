0b615d5d4485d86c81fe25721a3799ca53c596ef
Jarukrit <jarukritsripaploen@protonmail.com>
2023-05-05 23:39:10 +0700

    ...

--- src/net/itkmitl/room/portal/components/RoomLoader.java
+++ src/net/itkmitl/room/portal/components/RoomLoader.java
@@ -18,6 +18,7 @@ public class RoomLoader {
     public static HashMap<String, HashMap<String, Floor>> floorList = new HashMap<>();
     // 3.) HashMap<String(Building Name), HashMap<String(Floor Name), Floor(Object)>>
     public static boolean done = false;
+
     public RoomLoader() {
         loadRoom();
     }
@@ -29,7 +30,6 @@ public class RoomLoader {
                 FewQuery db = LaewTaeDB.getDB();
                 roomList = new RoomRepository(db).getRooms();
                 for (Room room : roomList) {
-//                    System.out.println("1.) "+room.getBuilding() + " " + room.getFloor() + " " + room.getName());
                     if (buildingList.containsKey(room.getBuilding())) {
                         buildingList.get(room.getBuilding()).add(room);
                     } else {
@@ -38,45 +38,27 @@ public class RoomLoader {
                     }
                 }
 
-                for (String building: buildingList.keySet()) {
+                for (String building : buildingList.keySet()) {
                     floorList.put(building, new HashMap<String, Floor>());
-//                    System.out.println("1.25) " + building);
                 }
 
 
-                for (String building: buildingList.keySet()){ // Get Key
-//                    floorList.put(building, new HashMap<String, Floor>());
-//                    System.out.println("1.5) " + building);
-                    for (Room room: buildingList.get(building)){ // Get Value
-//                        System.out.println("2.) RoomOBJ: "+ building + " " + room.getBuilding() + " " + room.getFloor() + " " + room.getName() + " " + floorList.containsKey(building));
-                        if(floorList.get(building).containsKey(room.getFloor())) {
-                            for (Map.Entry<String, Floor> insideMap: floorList.get(building).entrySet()){
-                                if (insideMap.getKey().equals(room.getFloor())){
+                for (String building : buildingList.keySet()) { // Get Key
+                    for (Room room : buildingList.get(building)) { // Get Value
+                        if (floorList.get(building).containsKey(room.getFloor())) {
+                            for (Map.Entry<String, Floor> insideMap : floorList.get(building).entrySet()) {
+                                if (insideMap.getKey().equals(room.getFloor())) {
                                     floorList.get(building).get(room.getFloor()).addRoom(room);
                                 }
                             }
-                        } else{
+                        } else {
                             Floor floor = new Floor();
                             floor.addRoom(room);
                             floorList.get(building).put(room.getFloor(), floor);
                         }
-                        System.out.println(floorList.get(building).size());
                     }
                 }
 
-//                for (String name: floorList.keySet()) {
-//                    String key = name.toString();
-//                    HashMap<String, Floor> value = floorList.get(name);
-//                    System.out.println("FloorList: " + key);
-//                    for (Map.Entry<String, Floor> data : value.entrySet()) {
-//                        String floor = data.getKey();
-//                        Floor myFloor = data.getValue();
-//                        System.out.println("  Fl: " + floor);
-//                        for (Map.Entry<String, Room> finalData : myFloor.getRooms().entrySet()) {
-//                            System.out.println("   Room: " + finalData.getKey());
-//                        }
-//                    }
-//                }
 
                 buildingList = null;
 
