0e2d238bbdfb8eb2c29e38d6d3d42a8b5616b143
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2023-05-05 11:39:14 +0700

    Feat: Assign FakeUser to Dashboard

--- src/net/itkmitl/room/portal/dashboard/Dashboard.java
+++ src/net/itkmitl/room/portal/dashboard/Dashboard.java
@@ -1,12 +1,17 @@
 package net.itkmitl.room.portal.dashboard;
 
+import net.itkmitl.room.libs.store.AppStore;
 import net.itkmitl.room.portal.Portal;
+import net.itkmitl.room.portal.components.FakeUser;
 
 public class Dashboard extends Portal {
     private final DashboardController controller;
+    private AppStore store = AppStore.getAppStore();
 
     public Dashboard() {
         super();
+
+        store.dispatch("user", FakeUser.getUser());
 
         DashboardView view = new DashboardView();
         this.controller = new DashboardController(view);
