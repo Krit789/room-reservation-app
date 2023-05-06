846421f1bceb6f455b07776e6584dacd8f4e708b
phatsanphonna <phatsanphon2004@outlook.com>
2023-05-06 20:04:14 +0700

    Feat: Clear ApplicationMain.java

--- src/net/itkmitl/room/ApplicationMain.java
+++ src/net/itkmitl/room/ApplicationMain.java
@@ -2,10 +2,6 @@ package net.itkmitl.room;
 
 public class ApplicationMain {
     public static void main(String[] args) {
-//        FewQuery db = RVDB.getDB();
-//        UserRepository userRepository = new UserRepository(db);
 
-//        User data = userRepository.getUserById(41);
-//        System.out.println(data.getCreatedOn());
     }
 }
