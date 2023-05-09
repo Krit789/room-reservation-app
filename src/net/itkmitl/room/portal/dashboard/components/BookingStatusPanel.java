1f68194a9d9bedb1fc63dffdf77a21473c3ea6a1
SarutaBamboo <65070211@kmitl.ac.th>
2023-05-09 23:40:02 +0700

    Add PieChart in DashBoard

--- src/net/itkmitl/room/portal/dashboard/components/BookingStatusPanel.java
+++ src/net/itkmitl/room/portal/dashboard/components/BookingStatusPanel.java
@@ -7,19 +7,28 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
-import javax.swing.JLabel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
 import net.itkmitl.room.portal.components.GBCBuilder;
 import net.itkmitl.room.portal.components.RoundedPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.DefaultPieDataset;
+
+
 
 public class BookingStatusPanel extends RoundedPanel {
 
     /**
-	 * 
-	 */
-	private static final long serialVersionUID = 84550694890228683L;
-	public JLabel titleLabel;
+     *
+     */
+    private static final long serialVersionUID = 84550694890228683L;
+    public JLabel titleLabel;
+    private JPanel piechart;
 
     public BookingStatusPanel() {
         super(30, 40, new Color(156, 199, 223), new Color(181, 191, 224));
@@ -35,5 +44,42 @@ public class BookingStatusPanel extends RoundedPanel {
         titleLabel.setFont(new Font("Cousin", Font.BOLD, 20));
 
         this.add(titleLabel, new GBCBuilder(GridBagConstraints.NONE, 0, 1, 0, 0).setAnchor(GridBagConstraints.NORTH));
+
+//        Pie chart
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        dataset.setValue("Available", 70);
+        dataset.setValue("Full", 30);
+
+        JFreeChart chart;
+        chart = ChartFactory.createPieChart(
+                        "",
+                dataset,
+                true,
+                true,
+                false
+        );;
+
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new Dimension(250, 250));
+        chartPanel.setBackground(new Color(0x0000005, true));
+
+        PiePlot plot = (PiePlot) chart.getPlot();
+
+
+        plot.setSectionPaint("Available", new Color(90, 140, 206));
+        plot.setSectionPaint("Full", new Color(0x101442));
+
+
+        piechart = new JPanel();
+        piechart.add(chartPanel);
+
+        this.add(piechart, new GBCBuilder(GridBagConstraints.BOTH, 0, 10, 0, 1).setAnchor(GridBagConstraints.CENTER));
+        piechart.setSize(new Dimension(50, 50));
+        piechart.setBackground(new Color(0x0343477, true));
+
+
+
+
+
     }
 }
