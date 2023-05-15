26237428463425aaf83ff6ea56ffdbfdc4523a84
japan0101 <japan.pon11@gmail.com>
2023-05-15 17:07:40 +0700

    Add Right panel and history box class
    How tf do I fixed the size of this shit

--- src/net/itkmitl/room/portal/content/components/history/HistoryRightPanel.java
+++ src/net/itkmitl/room/portal/content/components/history/HistoryRightPanel.java
@@ -1,14 +1,28 @@
 package net.itkmitl.room.portal.content.components.history;
 
+import net.itkmitl.room.portal.CardView;
 import net.itkmitl.room.portal.components.TransparentPanel;
 
 import javax.swing.*;
 
-public class HistoryRightPanel extends TransparentPanel {
+public class HistoryRightPanel extends CardView {
     private JLabel text;
+    public JScrollPane historyHolder;
+    public TransparentPanel reservationHistoryPanel;
+
+    private ReservationHistoryBox boxes;
     public HistoryRightPanel(){
         text = new JLabel("Contents go here!");
         add(text);
+        boxes = new ReservationHistoryBox("TestRoom", "12:30-16:30", "21 Jan 2023");
+        reservationHistoryPanel = new TransparentPanel();
+        reservationHistoryPanel.add(boxes);
+        historyHolder = new JScrollPane(reservationHistoryPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        historyHolder.setAlignmentY(JScrollPane.TOP_ALIGNMENT);
+        historyHolder.setOpaque(false);
+        historyHolder.getViewport().setOpaque(false);
+        historyHolder.setBorder(null);
+        add(historyHolder);
     }
 
 }
---
+++ src/net/itkmitl/room/portal/content/components/history/ReservationHistoryBox.java
@@ -0,0 +1,55 @@
+package net.itkmitl.room.portal.content.components.history;
+
+import net.itkmitl.room.portal.components.ButtonGradient;
+import net.itkmitl.room.portal.components.GBCBuilder;
+import net.itkmitl.room.portal.components.RoundedPanel;
+import net.itkmitl.room.portal.dashboard.components.BoxIcon;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class ReservationHistoryBox extends RoundedPanel {
+    public ButtonGradient feedBackBtn, cancelBtn;
+    private JLabel timeLabel, dateLabel, nameLabel;
+    private JPanel dataPanel;
+    private BoxIcon icon;
+    public ReservationHistoryBox(String name, String time, String date){
+        super(30, 40, Color.white);
+        setSize(1400, 250);
+
+        feedBackBtn = new ButtonGradient();
+        feedBackBtn.setText("FeedBack");
+        feedBackBtn.setActionCommand("FeedBack");
+        feedBackBtn.setColor1(Color.green);
+        feedBackBtn.setColor2(Color.green);
+
+        cancelBtn = new ButtonGradient();
+        cancelBtn.setText("Cancel");
+        cancelBtn.setActionCommand("Cancel");
+        cancelBtn.setColor1(Color.red);
+        cancelBtn.setColor2(Color.red);
+
+        nameLabel = new JLabel(name);
+        nameLabel.setFont(new Font("Cousine", Font.BOLD, 25));
+        dateLabel = new JLabel(date);
+        dateLabel.setFont(new Font("Cousine", Font.PLAIN, 20));
+        timeLabel = new JLabel(time);
+        timeLabel.setFont(new Font("Cousine", Font.PLAIN, 20));
+        dataPanel = new JPanel(new GridLayout(1, 5));
+        dataPanel.setOpaque(false);
+        icon = new BoxIcon(new ImageIcon("resource/icons/round-table.png"));
+
+        add(icon, BorderLayout.WEST);
+        add(Box.createHorizontalStrut(15));
+        dataPanel.add(nameLabel);
+        dataPanel.add(Box.createHorizontalStrut(5));
+        dataPanel.add(timeLabel);
+        dataPanel.add(Box.createHorizontalStrut(5));
+        dataPanel.add(dateLabel);
+        add(dataPanel, BorderLayout.CENTER);
+        add(Box.createHorizontalStrut(15));
+        add(feedBackBtn, BorderLayout.EAST);
+    }
+
+}
